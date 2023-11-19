95a201ad47206d3b01c21a00e8c7b849bc1c36ef
Alexander Ortiz <alex.ortiz.arevalo@gmail.com>
2023-11-18 20:33:22 -0500

    First version of basic math module

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Sum of two numbers.
+   * @example
+   * n1 = 1, n2 = 3, result = 4. 
+   * @param {*} n1 Number one of the sum.
+   * @param {*} n2 Number two of the sum.
+   * @returns 
+   */
+  sum: function (n1, n2) {
+    return n1 + n2;
+  },
+  /**
+   * Subtraction of two numbers.
+   * @example
+   * n1 = 1, n2 = 3, result = -2. 
+   * @param {*} n1 Number one of the subtraction.
+   * @param {*} n2 Number two of the subtraction.
+   * @returns 
+   */
+  subtract: function (n1, n2) {
+    return n1 - n2;
+  },
+  /**
+   * Multiplication of two numbers.
+   * @example
+   * n1 = 1, n2 = 3, result = 3. 
+   * @param {*} n1 Number one of the multiplication.
+   * @param {*} n2 Number two of the multiplication.
+   * @returns 
+   */
+  multiply: function (n1, n2) {
+    return n1 * n2;
+  },
+  /**
+   * division of two numbers.
+   * @example
+   * n1 = 1, n2 = 3, result = 0.33. 
+   * @param {*} n1 Number one of the division.
+   * @param {*} n2 Number two of the division.
+   * @returns 
+   */
+  divide: function (n1, n2) {
+    return n1 / n2;
+  },
+}
---
+++ tests.js
@@ -0,0 +1,6 @@
+const m = require('.');
+
+console.log(m.sum(1, 2));
+console.log(m.subtract(1, 2));
+console.log(m.multiply(1, 2));
+console.log(m.divide(1, 2));
